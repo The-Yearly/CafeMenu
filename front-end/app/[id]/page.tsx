8444db67a5652595a09dac93a44a73e418b708c9
The-Yearly <theyearlysclub@gmail.com>
2025-02-26 23:25:24 +0530

    s .·°՞(≧□≦)՞°·.

--- front-end/app/[id]/page.tsx
+++ front-end/app/[id]/page.tsx
@@ -2,7 +2,6 @@
 import { Food } from "../assets/interface/food";
 import { Swiper,SwiperSlide } from "swiper/react";
 import { EffectCoverflow,Autoplay, Pagination } from "swiper/modules";
-import Cart from "../components/cart";
 import AddButton from "../components/addButton";
 import Image from "next/image";
 import { useState,useEffect } from "react";
@@ -36,7 +35,6 @@ export default function Home(){
     const top5=data.slice(0,5)
     return(
       <>
-       <Cart/>
         <div>
           <p className="absolute xl-phone:top-[15vw] xl-phone:left-[5vw] xl-phone:text-[8vw] top-[15vw] left-[5vw] text-[8vw] font-playwrite">Malibu Cafe</p>
           <input className="absolute xl-phone:h-[15vw] xl-phone:top-[35vw] xl-phone:left-[2vw] xl-phone:w-[96vw] xl-phone:pl-[5vw] xl-phone:rounded-[3vw] h-[15vw] top-[35vw] left-[2vw] w-[96vw] pl-[5vw] rounded-[3vw] bg-white p-[1vw]" value={search} onChange={gotInput} placeholder="Search"/>
@@ -60,7 +58,7 @@ export default function Home(){
           grabCursor={true} 
           centeredSlides={true} 
           loop={true} 
-          spaceBetween={10}  
+          spaceBetween={0 }  
           slidesPerView={1.8}
           coverflowEffect={{
             rotate: 20,
@@ -74,21 +72,21 @@ export default function Home(){
             disableOnInteraction:false
           }}
           modules={[EffectCoverflow,Autoplay,Pagination]}           
-          className="xl-phone:top-[0vw] xl-phone:h-[50vw] xl-phone:w-[96vw] xl-phone:left-[4vw] top-[0vw] h-[50vw] w-[96vw] left-[4vw]">
+          className="xl-phone:top-[0vw] xl-phone:h-[50vw] xl-phone:w-[96vw] xl-phone:left-[4vw] top-[0vw] h-[50vw] b w-[100vw] left-[4vw]">
             {top5.map((food)=>
               <SwiperSlide className="bg-transparent rounded-[3vw]" key={food.item_id}>
-                <Image className="absolute 2xl:top-[-2vw] z-0 xl:top-[-2vw] md:top-[-2vw] 2xl:w-[10vw] lg:w-[10vw] lg:h-[10vw] md:w-[14vw] md:h-[14vw] sm:w-[23vw] sm:h-[23vw] sm:rounded-[15vw] sm:top-[-5vw] xl-phone:w-[45vw] xl-phone:h-[45vw] xl-phone:rounded-[30vw] xl-phone:top-[0vw] w-[40vw] h-[40vw] rounded-[20vw] top-[0vw]" alt={"Food Item"+food.item_id} width={1000} height={1000} src={food.item_image}/>
+                <Image className="absolute 2xl:top-[-2vw] z-0 xl:top-[-2vw] md:top-[-2vw] 2xl:w-[10vw] lg:w-[10vw] lg:h-[10vw] md:w-[14vw] md:h-[14vw] sm:w-[23vw] sm:h-[23vw] sm:rounded-[15vw] sm:top-[-5vw] xl-phone:w-[45vw] xl-phone:h-[45vw] xl-phone:rounded-[30vw] xl-phone:top-[0vw] xl-phone:left-0 left-[5vw] w-[45vw] h-[45vw] rounded-[25vw] top-[0vw]" alt={"Food Item"+food.item_id} width={1000} height={1000} src={food.item_image}/>
                 <AddButton item={food} location={"top5"}/>
               </SwiperSlide>
             )}
           </Swiper>
-          <div className="relative grid xl-phone:grid-cols-2 xl-phone:gap-4 xl-phone:top-[0vw] grid-cols-2 gap-4 top-[0vw]">
+          <div className="relative grid xl-phone:grid-cols-2 xl-phone:gap-4 xl-phone:top-[0vw] grid-cols-2 gap-y-10 top-[8  vw]">
               {(filteredData??data).map((food)=>
-                <div className="relative shadow-lg bg-white flex items-center justify-center 2xl:top-[9vw] 2xl:rounded-[2vw] 2xl:w-[14vw] 2xl:min-h-[17vw] 2xl:p-4 h-auto lg:p-8 lg:top-[9vw] lg:rounded-[3vw] lg:w-[18vw] lg:min-h-[23vw] md:p-8 md:top-[9vw] md:rounded-[4vw] md:w-[24vw] md:min-h-[30vw] sm:p-8 sm:top-[9vw] sm:rounded-[4vw] sm:w-[40vw] sm:min-h-[35vw] xl-phone:min-h-[65vw] xl-phone:h-auto xl-phone:p-8 xl-phone:top-[5vw] p-8 top-[0vw] rounded-[10vw] w-[47vw] min-h-[55vw] " key={food.item_id}>
-                  <Image className="absolute 2xl:top-[-2vw] z-0 xl:top-[-2vw] md:top-[-2vw] 2xl:w-[10vw] lg:w-[10vw] lg:h-[10vw] md:w-[14vw] md:h-[14vw] sm:w-[23vw] sm:h-[23vw] sm:rounded-[15vw] sm:top-[-5vw] xl-phone:w-[45vw] xl-phone:h-[45vw] xl-phone:rounded-[10vw] xl-phone:top-[0vw] w-[30vw] h-[30vw] rounded-[15vw] top-[-8vw]" alt={"Food Item"+food.item_id} width={1000} height={1000} src={food.item_image}/>
-                    <p className="absolute text-center text-black font-playwrite 2xl:text-[1.5vw] break-words  2xl:w-[10vw] 2xl:top-[8.5vw] lg:text-[2vw] lg:w-[15vw] lg:top-[11vw]  md:text-[2.5vw] md:w-[20vw] md:top-[13vw]  sm:text-[3vw] sm:w-[20vw] sm:top-[18vw]  xl-phone:text-[4vw] xl-phone:w-[35vw] xl-phone:top-[48vw] text-[4.5vw] w-[30vw] top-[24vw] ">{food.item_name}</p>
-                    <p className="absolute text-center 2xl:top-[13.7vw] 2xl:text-[1.5vw] font-playwrite lg:text-[2vw] lg:top-[19vw] md:text-[2.5vw] md:top-[24vw] sm:text-[3vw] sm:top-[29vw] xl-phone:text-[4vw] xl-phone:top-[60vw] text-[4.5vw] top-[43vw]">{food.item_cost}</p>
-                    <AddButton item={food} location="item"/>
+                <div className="relative shadow-lg bg-white flex items-center justify-center 2xl:top-[9vw] 2xl:rounded-[2vw] 2xl:w-[14vw] 2xl:min-h-[17vw] 2xl:p-4 h-auto lg:p-8 lg:top-[9vw] lg:rounded-[3vw] lg:w-[18vw] lg:min-h-[23vw] md:p-8 md:top-[9vw] md:rounded-[4vw] md:w-[24vw] md:min-h-[30vw] sm:p-8 sm:top-[9vw] sm:rounded-[4vw] sm:w-[40vw] sm:min-h-[35vw] xl-phone:min-h-[65vw] xl-phone:h-auto xl-phone:p-8 xl-phone:top-[5vw] p-8 top-[0vw] left-[3vw] rounded-[10vw] w-[47vw] min-h-[70vw] " key={food.item_id}>
+                  <Image className="absolute 2xl:top-[-2vw] z-0 xl:top-[-2vw] md:top-[-2vw] 2xl:w-[10vw] lg:w-[10vw] lg:h-[10vw] md:w-[14vw] md:h-[14vw] sm:w-[23vw] sm:h-[23vw] sm:rounded-[15vw] sm:top-[-5vw] xl-phone:w-[45vw] xl-phone:h-[45vw] xl-phone:rounded-[10vw] xl-phone:top-[0vw] w-[45vw] h-[45vw] rounded-[10vw] top-[0vw]" alt={"Food Item"+food.item_id} width={1000} height={1000} src={food.item_image}/>
+                    <p className="absolute text-center text-black font-playwrite 2xl:text-[1.5vw] break-words  2xl:w-[10vw] 2xl:top-[8.5vw] lg:text-[2vw] lg:w-[15vw] lg:top-[11vw]  md:text-[2.5vw] md:w-[20vw] md:top-[13vw]  sm:text-[3vw] sm:w-[20vw] sm:top-[18vw]  xl-phone:text-[4vw] xl-phone:w-[35vw] xl-phone:top-[48vw] text-[4.5vw] w-[30vw] top-[45vw] ">{food.item_name}</p>
+                    <p className="absolute text-center 2xl:top-[13.7vw] 2xl:text-[1.5vw] font-playwrite lg:text-[2vw] lg:top-[19vw] md:text-[2.5vw] md:top-[24vw] sm:text-[3vw] sm:top-[29vw] xl-phone:text-[4vw] xl-phone:top-[60vw] text-[4.5vw] top-[60vw]">{food.item_cost}</p>
+                    <AddButton item={food} location={"item"}/>
                   </div>
                 )}
           </div>
--- front-end/app/components/cart.tsx
+++ front-end/app/components/cart.tsx
@@ -1,5 +1,5 @@
+'use client'
 import { Swiper,SwiperSlide } from "swiper/react";
-import { EffectCoverflow,Autoplay, Pagination } from "swiper/modules";
 import cart from "@/app/assets/images/Cart.png"
 import Image from "next/image";
 import AddButton from "./addButton";
@@ -9,6 +9,18 @@ import 'swiper/css/bundle';
 export default function Cart(){
     const [cartData,setCartData]=useState<Food[]|null>(null)
     const [iscartShowing,setCartShowing]=useState(false)
+    useEffect(()=>{const getCart=()=>{
+        const cart=sessionStorage.getItem("cart")
+        const cartdets=sessionStorage.getItem("cartDets")
+        if(cart==null){
+          sessionStorage.setItem("cart","{}")
+        }
+        if(cartdets==null){
+          sessionStorage.setItem("cartDets","[]")
+        }
+        }
+        getCart()
+        })
     useEffect(()=>{const fetchCart=()=>{
         const cartDets=sessionStorage.getItem("cartDets")
         if(cartDets!=null){
@@ -20,13 +32,13 @@ export default function Cart(){
         return(
             <>
              <Image src={cart} className="absolute xl-phone:top-[5vw] xl-phone:right-[5vw] xl-phone:w-[10vw] top-[5vw] right-[5vw] w-[10vw] " onClick={()=>{setCartShowing(true)}} height={1000} width={1000} alt="Cart"/>
-            <div className={iscartShowing?"fixed top-0 xl-phone:right-0 xl-phone:z-10 xl-phone:w-[30vw] xl-phone:h-[100%] backdrop-filter backdrop-blur-sm bg-opacity-20 bg-slate-100 border border-gray-100":"hidden"}>
+            <div className={iscartShowing?"fixed top-0 right-0 z-10 xl-phone:w-[30vw] w-[30vw] h-[100%] backdrop-filter backdrop-blur-sm bg-opacity-20 bg-slate-100 border border-gray-100":"hidden"}>
             <p className="font-times text-gray-500" onClick={()=>{setCartShowing(false)}}>X</p>
-            <Swiper className="xl-phone:top-[5vw] xl-phone:h-[200vw]" slidesPerView={6}  direction={"vertical"}>
+            <Swiper className="xl-phone:top-[5vw] xl-phone:h-[200vw] top-[2 vw] h-[180vw]" slidesPerView={6}  direction={"vertical"}>
                 {cartData?.map((food)=>
                 <SwiperSlide key={food.item_id}>
                   <AddButton item={food} location="cart"/>
-                  <Image className="xl-phone:w-[30vw] xl-phone:rounded-[30vw] xl-phone:h-[30vw] " src={food.item_image} width={1000} height={1000} alt="Food"/>
+                  <Image className="xl-phone:w-[30vw] xl-phone:rounded-[30vw] xl-phone:h-[30vw] w-[30vw] rounded-[30vw] h-[30vw] " src={food.item_image} width={1000} height={1000} alt="Food"/>
                 </SwiperSlide>)}
             </Swiper>
         </div>
