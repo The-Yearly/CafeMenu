beea89b1354b8a6a1150464c975d24c631bc4014
Swayam Agrahari <swayamagrahari025@gmail.com>
2025-03-03 17:49:27 +0530

    feat:added backend routes, types and db modals

--- server/prisma/migrations/20250222214734_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `item_subcategory` to the `Items` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Items" ADD COLUMN     "item_subcategory" TEXT NOT NULL;
